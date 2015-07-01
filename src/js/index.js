9197174bbfb9d38637dcc5536582819fcc4cd516
PolarGom <phkaa@naver.com>
2015-07-01 18:21:28 +0900

    Slider PG 불필요한 이미지 삭제

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,4 @@ $(document).ready(function() {
 
 	$('#first_slider').sliderPG({});
 	//$('#first_slider').sliderPG('gotoPage', 2);
-	$('#sec_slider').sliderPG({width: 468, autoSlide: true, autoIntervalDely: 2000});
 });
